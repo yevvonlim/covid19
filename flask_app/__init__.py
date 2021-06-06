b9941bdfad40f640c1ba428bd90b508fff7f35da
three0-s <ga06033@o365.yonsei.ac.kr>
2021-06-06 11:29:19 +0000

    210606 apply bootstrap theme

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,14 +1,10 @@
-
 from flask import Flask
-
 
 def create_app():
     app = Flask(__name__)
-
-
-    # blueprint
+    
+    # blue print
     from views import main_views
     app.register_blueprint(main_views.bp)
-
-
+    
     return app
--- views/main_views.py
+++ views/main_views.py
@@ -2,13 +2,12 @@ from flask import Blueprint, render_template
 
 
 bp = Blueprint('main', __name__, url_prefix='/')
-question = ["어디로 가시나요?", "얼마나 머무르시나요?", "당신의 코로나19 위험도 점수는"]
+# question = ["어디로 가시나요?", "얼마나 머무르시나요?", "당신의 코로나19 위험도 점수는"]
 
 
 @bp.route('/')
 def syte_inspection():
-    return render_template('question/question_list.html',
-                           question_list=question)
+    return render_template('index.html')
 
 
 """
