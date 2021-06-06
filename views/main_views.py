b7d16b01b3d485c49fa6357f73f47ce2a59b1b0b
three0-s <ga06033@o365.yonsei.ac.kr>
2021-06-06 14:53:26 +0000

    210606 add scikit-learn model

--- views/main_views.py
+++ views/main_views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
+import model.covid_danger as main
 
 bp = Blueprint('main', __name__, url_prefix='/')
 # question = ["어디로 가시나요?", "얼마나 머무르시나요?", "당신의 코로나19 위험도 점수는"]
@@ -10,13 +10,15 @@ def syte_inspection():
     return render_template('index.html')
 
 
-"""
+
 @bp.route('/', methods=['POST'])
 def test_print():
-    score = request.form['text']
+    location = request.form['location']
+    time = request.form['time']
+    
     return render_template('question/question_list.html',
-                        question_list=question, score=score)
-"""
+                      question_list=question, score=score)
+
 
 
 @bp.route('/ing')
