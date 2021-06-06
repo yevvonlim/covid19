3e6bd5724d814ba6b39eda958a036f042b249ade
three0-s <ga06033@o365.yonsei.ac.kr>
2021-06-06 07:53:01 +0000

    210606 relocate static folder

--- views/main_views.py
+++ views/main_views.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 question = ["어디로 가시나요?", "얼마나 머무르시나요?", "당신의 코로나19 위험도 점수는"]
 
+
 @bp.route('/')
 def syte_inspection():
-    return render_template('question/question_list.html', question_list=question)
+    return render_template('question/question_list.html',
+                           question_list=question)
+
 
 """
 @bp.route('/', methods=['POST'])
 def test_print():
     score = request.form['text']
-    return render_template('question/question_list.html', question_list=question, score=score)
+    return render_template('question/question_list.html',
+                        question_list=question, score=score)
 """
 
 
