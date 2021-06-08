dd4da0bdcf96246c10494b57632d25b376371e2c
three0-s <ga06033@o365.yonsei.ac.kr>
2021-06-08 11:43:37 +0000

    210608  link with model

--- views/main_views.py
+++ views/main_views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, request
-import model.covid_danger as main
+from flask_app.model import covid_danger as cd
 
 bp = Blueprint('main', __name__, url_prefix='/')
 # question = ["어디로 가시나요?", "얼마나 머무르시나요?", "당신의 코로나19 위험도 점수는"]
-
+path = r"/workspace/covid19/flask_app/model/model.pkl"
+model = cd.load_model(path)
 
 @bp.route('/')
 def syte_inspection():
@@ -13,11 +14,14 @@ def syte_inspection():
 
 @bp.route('/', methods=['POST'])
 def test_print():
-    location = request.form['location']
-    time = request.form['time']
+    location = request.form['location'].split(',')
+    time = float(request.form['time'])
+    x, y = float(location[1]), float(location[0])
     
-    return render_template('question/question_list.html',
-                      question_list=question, score=score)
+    score = cd.calc_critical_score(model, x, y, time=time)
+    
+    return render_template('index.html',
+                      num=str(score))
 
 
 
