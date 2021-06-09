33535520a43d145d6a2ed9cc6e732277a943f1aa
Yewon Lim <ga06033@yonsei.ac.kr>
2021-06-09 10:23:01 +0000

    210609 merge

--- views/main_views.py
+++ views/main_views.py
@@ -3,7 +3,7 @@ from flask_app.model import covid_danger as cd
 
 bp = Blueprint('main', __name__, url_prefix='/')
 # question = ["어디로 가시나요?", "얼마나 머무르시나요?", "당신의 코로나19 위험도 점수는"]
-path = r"/workspace/covid19/flask_app/model/model.pkl"
+path = r"/home/COVID19danger/mysite/flask_app/model/model.pkl"
 model = cd.load_model(path)
 
 @bp.route('/')
@@ -15,11 +15,11 @@ def syte_inspection():
 @bp.route('/', methods=['POST'])
 def test_print():
     location = request.form['coordi'].split(',')
-   
+
     time = float(request.form['time'])
     x, y = float(location[1]), float(location[0])
     # print(x, y, time)
-    
+
     score = cd.calc_critical_score(model, y, x, time=time)
     # print(score)
     return format(score, ".1f")
